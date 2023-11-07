6a26505a5e0db92c142089d2d9d2d210b5e18a27
Alexander Rebai <alexander-rebai@hotmail.com>
2023-11-07 09:37:18 +0100

    create navbar and footer

---
+++ src/components/footer.tsx
@@ -0,0 +1,15 @@
+const Footer = () => {
+  return (
+    <footer className="bg-white border-t">
+      <div>
+        <div className="mx-auto py-10">
+          <p className="text-center text-xs text-black">
+            &copy; 2023 FakeStore, Inc. All rights reserved.
+          </p>
+        </div>
+      </div>
+    </footer>
+  );
+};
+
+export default Footer;
